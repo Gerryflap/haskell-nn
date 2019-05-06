7e54eee9b8b0a4fe6560caea2978f02dfa730b93
gerryflap <gerry2211@gmail.com>
2019-05-06 16:02:08 +0200

    Updated example

--- haskell-nn/app/Main.hs
+++ haskell-nn/app/Main.hs
@@ -16,8 +16,8 @@ ydata = Matrix [[0.0], [1.0], [1.0], [0.0]]
 nn = Layer $ Sequential [
         Layer $ Dense (zeros 2 20) (zeros 20 0),
         Layer $ Activation relu,
---        Layer $ Dense (zeros 20 20) (zeros 20 0),
---        Layer $ Activation relu,
+        Layer $ Dense (zeros 20 20) (zeros 20 0),
+        Layer $ Activation relu,
         Layer $ Dense (zeros 20 1) (zeros 1 0),
         Layer $ Activation sigmoid
     ]
@@ -25,14 +25,15 @@ nn = Layer $ Sequential [
 model = Model nn mse (Optimizer $ SGD 0.1)
 
 train m 0 = "Done.\n" ++ "Final output: " ++ (show $ predictBatch m xdata) ++ "\n Model params:" ++ (show (getparams $ getLayer m))
-train m batches = "Loss: " ++ (show loss) ++ "\n" ++ train nm (batches-1)
+train m batches | batches `mod` 100 == 0 = "Loss: " ++ (show loss) ++ "\n" ++ train nm (batches-1)
+                | otherwise = train nm (batches-1)
     where
         (loss, nm) = trainBatch m xdata ydata
 
 main :: IO ()
 main = do
     stdgen <- getStdGen
-    let rands = randomRs (-3.0, 3.0) stdgen :: [Float]
+    let rands = randomRs (-0.3, 0.3) stdgen :: [Float]
     let (nrands, imodel) = initM rands model
 
-    putStrLn $ train imodel 1000
+    putStrLn $ train imodel 10000
